e19d83eb7493067128d031c00c7093c0d66f4947
Lima <lmimsra07@gmail.com>
2019-12-15 20:40:34 +0900

    個別ページ作成

---
+++ lib/pages/my_events.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyEventsScreen extends StatelessWidget {
+  MyEventsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return null;
+  }
+}
