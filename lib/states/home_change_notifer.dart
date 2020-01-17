95bb4aa361bd844f6fc9c0fd2767a8083011035d
Lima <lmimsra07@gmail.com>
2020-01-18 00:14:34 +0900

    トップページの状態保持を外部ファイルに変更

--- lib/states/home_change_notifer.dart
+++ lib/states/home_change_notifer.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class HomeChangeNotifier extends ChangeNotifier {
   List _myEvents;
   List _checkedEvents;
+  int _homePageIndex;
 
   HomeChangeNotifier() {
     // 各Listの初期化
     _myEvents=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
     _checkedEvents=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
+    _homePageIndex=0;
     updateMyEvents();
     updateCheckedEvents();
   }
@@ -17,6 +19,12 @@ class HomeChangeNotifier extends ChangeNotifier {
 
   List get checkedEvents => _checkedEvents;
 
+  int get homePageIndex  => _homePageIndex;
+
+  void updateHomePageIndex(int value) {
+    _homePageIndex = value;
+    notifyListeners();
+  }
   void updateMyEvents(){
     //イベント更新処理
     notifyListeners();
