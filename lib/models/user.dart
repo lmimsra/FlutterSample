71b7c0e438b3812fcddc14bea9155488678d1068
Lima <lmimsra07@gmail.com>
2020-02-19 09:48:35 +0900

    Json対応する処理を追加

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -4,6 +4,7 @@ class User {
   final String id;
   final String name;
   final String imageUrl;
+  String email;
   String description;
   int sex;
   DateTime birthday;
@@ -20,4 +21,29 @@ class User {
     this.age,
     this.createdAt,
   });
+
+  // json形式からclassに変換
+  User.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        name = json['name'],
+        imageUrl = json['imageUrl'],
+        email = json['email'],
+        description = json['description'],
+        sex = json['sex'],
+        birthday = json['birthday'],
+        age = json['age'],
+        createdAt = json['createdAt'];
+
+  // json形式にマッピング
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        'name': name,
+        "imageUrl": imageUrl,
+        'email': email,
+        'description': description,
+        'sex': sex,
+        'birthday': birthday,
+        'age': age,
+        'createdAt': createdAt,
+      };
 }
